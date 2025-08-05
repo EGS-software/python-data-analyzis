48b75cd8ac531655e0cf05e6ed8366c62118fa22
jawc-05 <joaoalfredowc@gmail.com>
2025-08-05 00:31:43 -0300

    feat: added conjuntos.py

---
+++ Aula1/conjuntos.py
@@ -0,0 +1,10 @@
+# Criando conjuntos
+conjunto = set([1,2,3,1,2]) # set([1,2,3])
+conjunto = {1,2,3,1,2} #set([1,2,3])
+
+#Operações
+conj_a = {1, 2, 3}
+conj_b = {2, 3, 4}
+
+conj_a.union(conj_b)    #{1,2,3,4}
+conj_a.intersection(conj_b) #{2,3}
