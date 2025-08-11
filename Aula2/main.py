b14b6dad9db5e3b7a9339c66deea3d6ea1cda926
JaoVicy <filipimjoa@gmail.com>
2025-08-11 19:53:36 -0300

    chore: format with black

--- Aula2/main.py
+++ Aula2/main.py
@@ -1,33 +1,39 @@
 def coletar_dados_recinto():
-    print('-----DADOS DO RECINTO-----')
+    print("-----DADOS DO RECINTO-----")
     comprimento = float(input("Digite o comprimento da sala (em m): "))
     largura = float(input("Digite a largura da sala (em m): "))
-    altura = float(input('Digite o pé direito da sala (em m): '))
+    altura = float(input("Digite o pé direito da sala (em m): "))
     volume = comprimento * largura * altura
-    posicao = int(input('A sala está entre andares (1) ou sob o telhado (2)? '))
-    print('Dados armazenados\n')
+    posicao = int(input("A sala está entre andares (1) ou sob o telhado (2)? "))
+    print("Dados armazenados\n")
     return volume, posicao
 
 
 def coletar_dados_aberturas():
-    print('-----DADOS DE ABERTURAS-----')
-    numJanela = int(input('Quantas janelas existem na sala? '))
-    areaJanela = float(input('Qual a área ocupada por essas janelas (em m2)? '))
-    numPortas = int(input('Quantas portas existem na sala? '))
-    areaPorta = float(input('Qual a área ocupada por portas (em m2)? '))
-    print('Dados armazenados\n')
+    print("-----DADOS DE ABERTURAS-----")
+    numJanela = int(input("Quantas janelas existem na sala? "))
+    areaJanela = float(input("Qual a área ocupada por essas janelas (em m2)? "))
+    numPortas = int(input("Quantas portas existem na sala? "))
+    areaPorta = float(input("Qual a área ocupada por portas (em m2)? "))
+    print("Dados armazenados\n")
     return areaJanela, areaPorta
 
 
 def coletar_dados_adicionais():
-    print('-----DADOS ADICIONAIS-----')
-    numPessoas = int(input('Quantas pessoas ficarão na sala? '))
-    numEquip = int(input('Qual a potência elétrica (em W) somada de todos os equipamentos da sala? '))
-    print('Dados armazenados\n')
+    print("-----DADOS ADICIONAIS-----")
+    numPessoas = int(input("Quantas pessoas ficarão na sala? "))
+    numEquip = int(
+        input(
+            "Qual a potência elétrica (em W) somada de todos os equipamentos da sala? "
+        )
+    )
+    print("Dados armazenados\n")
     return numPessoas, numEquip
 
 
-def calcular_carga_termica(volume, posicao, areaJanela, areaPorta, numPessoas, numEquip):
+def calcular_carga_termica(
+    volume, posicao, areaJanela, areaPorta, numPessoas, numEquip
+):
     # Tabela 1
     if posicao == 1:
         recinto = volume * 16
@@ -45,16 +51,20 @@ def calcular_carga_termica(volume, posicao, areaJanela, areaPorta, numPessoas, n
     return recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU
 
 
-def exibir_resultados(recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU):
-    print('-----RESULTADOS DO LEVANTAMENTO-----')
-    print(f'Recinto: {recinto} kcal/h')
-    print(f'Janelas: {janela} kcal/h')
-    print(f'Portas: {porta} kcal/h')
-    print(f'Pessoas: {pessoas} kcal/h')
-    print(f'Equipamentos elétricos: {aparelhos} kcal/h')
-    print(f'\nCarga térmica (kcal/h): {cargaTermica}')
-    print(f'Carga térmica (em BTUs): {cargaBTU}')
-    print(f'\nPara refrigerar essa sala adequadamente, é necessário um AC de {cargaBTU} BTUs')
+def exibir_resultados(
+    recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU
+):
+    print("-----RESULTADOS DO LEVANTAMENTO-----")
+    print(f"Recinto: {recinto} kcal/h")
+    print(f"Janelas: {janela} kcal/h")
+    print(f"Portas: {porta} kcal/h")
+    print(f"Pessoas: {pessoas} kcal/h")
+    print(f"Equipamentos elétricos: {aparelhos} kcal/h")
+    print(f"\nCarga térmica (kcal/h): {cargaTermica}")
+    print(f"Carga térmica (em BTUs): {cargaBTU}")
+    print(
+        f"\nPara refrigerar essa sala adequadamente, é necessário um AC de {cargaBTU} BTUs"
+    )
 
 
 def main():
@@ -62,12 +72,16 @@ def main():
     areaJanela, areaPorta = coletar_dados_aberturas()
     numPessoas, numEquip = coletar_dados_adicionais()
 
-    recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU = calcular_carga_termica(
-        volume, posicao, areaJanela, areaPorta, numPessoas, numEquip
+    recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU = (
+        calcular_carga_termica(
+            volume, posicao, areaJanela, areaPorta, numPessoas, numEquip
+        )
     )
 
-    exibir_resultados(recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU)
+    exibir_resultados(
+        recinto, janela, porta, pessoas, aparelhos, cargaTermica, cargaBTU
+    )
 
 
-if __name__ == '__main__': # Entry point for the script
+if __name__ == "__main__":  # Entry point for the script
     main()
