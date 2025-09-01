18dfd565010d5fe35d22d4a0c57342a64475889d
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-01 20:03:55 -0300

    fix: improving the main.py

--- Aula4/main.py
+++ Aula4/main.py
@@ -1,17 +1,18 @@
 # Ler arquivo:
-arquivo = open("exemplo.txt", 'r')
-conteudo = arquivo.read()
-print(conteudo)
-arquivo.close()
+arquivo = open("exemplo.txt", 'r');
+conteudo = arquivo.read();
+print(conteudo);
+arquivo.close();
 
 # Forma na norma padrão:
 with open('exemplo.txt', 'r') as arquivo:
-  conteudo = arquivo.read()
-  print(conteudo)
+  conteudo = arquivo.read();
+  print(conteudo);
 
 # Escrever arquivo:
 with open(dados.txt, 'w') as dados:
-  arquivo.write("Olá garotos")
-  arquivo.write("Sou um garotão de software)
+  arquivo.write("Olá garotos");
+  arquivo.write("Sou um garotão de software");
+
 
 
