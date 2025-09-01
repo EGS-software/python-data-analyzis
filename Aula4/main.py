496ee38c251d91854e6cc836bac48c36fa717042
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-01 20:12:25 -0300

    feat: adding csv

--- Aula4/main.py
+++ Aula4/main.py
@@ -14,5 +14,15 @@ with open(dados.txt, 'w') as dados:
   arquivo.write("Olá garotos");
   arquivo.write("Sou um garotão de software");
 
+# Manipulação de arquivo CSV
+
+import csv
+
+#Abrir o arquivo CSV para leitura
+with open('dados-csv.csv', 'r', 'utf-8') as entrada:
+  leitor_csv = csv.reader(entrada);
+
+
+
 
 
