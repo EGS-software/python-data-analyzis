e16e7ee32f1d33432cb9f6c189fa8c237e773e1b
João V. Benetti F. <140014904+JaoVicy@users.noreply.github.com>
2025-09-01 19:54:11 -0300

    feat: update main.py with normative format

--- Aula4/main.py
+++ Aula4/main.py
@@ -2,3 +2,8 @@ arquivo = open("exemplo.txt", 'r')
 conteudo = arquivo.read()
 print(conteudo)
 arquivo.close()
+
+# Forma na norma padrão:
+with open('exemplo.txt', 'r') as arquivo:
+  conteudo = arquivo.read()
+  print(conteudo)
