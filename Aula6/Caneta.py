20e8c8889bd27f39715319e0e2e1347dc4d360d4
jawc-05 <joaoalfredowc@gmail.com>
2025-09-15 20:15:54 -0300

    feat: adding class Caneta

---
+++ Aula6/Caneta.py
@@ -0,0 +1,6 @@
+class Caneta:
+    def __init__(self, color, model, carga, tampada):
+        self.color:str = color
+        self.model:str = model
+        self.carga:int = carga
+        self.tampada:bool = tampada
