521f2470dd4d2c97b145ca8222eca392ff4ec0b1
jawc-05 <joaoalfredowc@gmail.com>
2025-09-15 20:17:16 -0300

    fix: fixed the type of the carga

--- Aula6/Caneta.py
+++ Aula6/Caneta.py
@@ -2,5 +2,5 @@ class Caneta:
     def __init__(self, color, model, carga, tampada):
         self.color:str = color
         self.model:str = model
-        self.carga:int = carga
+        self.carga:str = carga
         self.tampada:bool = tampada
