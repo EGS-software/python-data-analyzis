4ab9c2f388973bd930bb993d97074a24b7016e57
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-15 20:08:28 -0300

    feat: added parameters

--- Aula6/ControleRemoto.py
+++ Aula6/ControleRemoto.py
@@ -10,13 +10,15 @@ def alterar_temperatura(self, botao):
         print ("Aumentando a temperatura")
     elif botao == "-":
         print ("Diminuindo a temperatura")
-    
 
 controle_remoto1 = ControleRemoto("branco", "8cm", "4cm", "1cm")
 print(controle_remoto1.color, controle_remoto1.height, controle_remoto1.largura, controle_remoto1.profundidade)
 
 controle_remoto2 = ControleRemoto("cinza", "10cm", "3cm", "2cm")
 print(controle_remoto1.color, controle_remoto1.height, controle_remoto1.largura, controle_remoto1.profundidade)
+
+controle_remoto1.alterar_temperatura("+")
+controle_remoto1.alterar_temperatura("-")
 
 # Caracteristicas
 #------
