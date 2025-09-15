a35a04bc77dd50f49a3617a7f33a1c341e426275
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-15 20:04:20 -0300

    feat: add function alterar_temperatura

--- Aula6/ControleRemoto.py
+++ Aula6/ControleRemoto.py
@@ -5,11 +5,18 @@ class ControleRemoto:
         self.largura = largura
         self.profundidade = profundidade
 
+def alterar_temperatura(self, botao):
+    if botao == "+":
+        print ("Aumentando a temperatura")
+    elif botao == "-":
+        print ("Diminuindo a temperatura")
+    
+
 controle_remoto1 = ControleRemoto("branco", "8cm", "4cm", "1cm")
-print(controle_remoto1.color)
+print(controle_remoto1.color, controle_remoto1.height, controle_remoto1.largura, controle_remoto1.profundidade)
 
 controle_remoto2 = ControleRemoto("cinza", "10cm", "3cm", "2cm")
-print(controle_remoto1.color)
+print(controle_remoto1.color, controle_remoto1.height, controle_remoto1.largura, controle_remoto1.profundidade)
 
 # Caracteristicas
 #------
