55e84253d33a36a487794b653497d7443a8f6fab
jawc-05 <joaoalfredowc@gmail.com>
2025-09-15 20:44:39 -0300

    feat: adding parameters  and FIXES

--- Aula6/Caneta.py
+++ Aula6/Caneta.py
@@ -1,5 +1,5 @@
-class caneta:
-    def __init__(self, cor, ponta, carga, tampada):
+class Caneta:
+    def __init__(self, cor, modelo, carga, tampada):
         self.cor = cor
         self.modelo = modelo
         self.carga = carga
@@ -14,9 +14,16 @@ class caneta:
 try:
     c1 = Caneta("Vermelha", "WBM-7", 100, False)
     print(c1.cor, c1.modelo, c1.carga, c1.tampada)
-
 except Exception:
     print("Erro ao criar ou usar c1")
+
+try:
+    c2 = Caneta("Azul", "QB-250", 80, True)
+    print(c2.cor, c2.modelo, c2.carga, c2.tampada)
+    c2.rabiscar()
+except Exception:
+    print("Erro ao criar ou usar c2")
+
 
     def tampar(self):
         self.tampada = True
