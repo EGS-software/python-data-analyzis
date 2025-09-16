317e45d42897ee831d66e9e9f79c251288779893
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-15 21:21:21 -0300

    fix: fixing def abrirConta

--- Aula6/ContaBanco.py
+++ Aula6/ContaBanco.py
@@ -1,14 +1,16 @@
-def __init__ (self, numeroConta, tipo, dono, saldo, status):
-        self.numeroConta = numeroConta
+class ContaBanco:
+     
+    def __init__ (self, numeroConta, tipo, dono, saldo, status):
+        self.numeroConta = numeroConta 
         self.tipo = tipo
         self.dono = dono
         self.saldo = saldo
         self.status = status
 
-def abrirConta(self, tipo):
-    self.tipo = tipo
-    self.status = True
-    if tipo == "CC":
-        self.saldo = 50
-    elif tipo == "CP":
-        self.saldo = 150    
+    def abrirConta(self, tipo):
+        self.tipo = tipo
+        self.status = True
+        if tipo == "CC":
+            self.saldo = 50
+        elif tipo == "CP":
+            self.saldo = 150    
