a032d0418ba2e7565d178d903e3dc003a6036396
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-15 21:15:14 -0300

    feat: create class ContaBanco

---
+++ Aula6/ContaBanco.py
@@ -0,0 +1,6 @@
+def __init__ (self, numeroConta, tipo, dono, saldo, status):
+        self.numeroConta = numeroConta
+        self.tipo = tipo
+        self.dono = dono
+        self.saldo = saldo
+        self.status = status
