c09a801168e4a50f9c2ae15ce1d7d9bf946aa9ee
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-15 21:17:53 -0300

    feat: adding function abrirConta

--- Aula6/ContaBanco.py
+++ Aula6/ContaBanco.py
@@ -4,3 +4,11 @@ def __init__ (self, numeroConta, tipo, dono, saldo, status):
         self.dono = dono
         self.saldo = saldo
         self.status = status
+
+def abrirConta(self, tipo):
+    self.tipo = tipo
+    self.status = True
+    if tipo == "CC":
+        self.saldo = 50
+    elif tipo == "CP":
+        self.saldo = 150    
