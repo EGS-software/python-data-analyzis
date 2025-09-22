4ce39eef158695f10d7dff849170274cd2c2cfb5
jawc-05 <joaoalfredowc@gmail.com>
2025-09-22 20:49:06 -0300

    feat: adding the try

--- Aula7/conexao.py
+++ Aula7/conexao.py
@@ -1,3 +1,13 @@
 from getpass import getpass
 from mysql.connector import connect
 
+try:
+    with connect(
+        host="localhost",
+        user=input("Usuário: "),
+        password=getpass("Senha: ")
+    ) as connection:
+        print("Conexão estabelecida!")
+
+except:
+    pass
