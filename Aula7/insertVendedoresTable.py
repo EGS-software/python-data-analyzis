6dd3da3f7bf3a6e529b858ecd4b740517301021b
jawc-05 <joaoalfredowc@gmail.com>
2025-09-22 22:06:15 -0300

    feat: adding def inserir_vendedores

---
+++ Aula7/insertVendedoresTable.py
@@ -0,0 +1,16 @@
+from conexao import usar_conexao
+
+def inserir_vendedores(connection):
+    #Usamos 3 """ para permitir quebras de linha na query"
+    insert_query = """ 
+    INSERT INTO Vendedores(Nome) VALUES
+        {'João Alfredo Williges Cunha'}
+    """
+    with connection.cursor() as cursor:
+        #Aqui deve-se colocar o nome do banco correto após o USE
+        cursor.execute("USE bdSegunda")
+        cursor.execute(insert_query)
+        connection.commit()
+        print("Vendedor inserido com sucesso!")
+
+usar_conexao(inserir_vendedores)
