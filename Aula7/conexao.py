1d262dfe4f9246ff0af9806142dc4a858668be34
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-22 21:42:03 -0300

    feat: update conexao.py

--- Aula7/conexao.py
+++ Aula7/conexao.py
@@ -1,23 +1,15 @@
 from getpass import getpass
-from mysql.connector import connect
+from mysql.connector import connect, Error
 
-try:
-    with connect(
-        host="localhost",
-        user=input("Usuário: "),
-        password=getpass("Senha: ")
-    ) as connection:
-        print("Conexão estabelecida!")
+def usar_conexao(acao):
+    try:
+        with connect(
+            host="localhost",
+            user=input("Usuário: "),
+            password=getpass("Senha: ")
+        ) as connection:
+            print("Conexão estabelecida!", connection)
+            return acao(connection)
 
-        print("BD existentes no servidor: ")
-        show_db_query = "SHOW DATABASES"
-        #Cria o objeto cursor a partir da conexão
-        with connection.cursor() as cursor:
-            #Executa a query
-            cursor.execute(show_db_query)
-            #Percorre o resultado da query
-            for db in cursor:
-                print(db[0])
-
-except:
-    print("Ocorreu um erro ao tentar se conectar ao banco de dados.")
+    except Error as e:
+        print("Ocorreu um erro ao tentar se conectar ao banco de dados.")
