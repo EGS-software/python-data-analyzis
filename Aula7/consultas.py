69d9049eb85ad296d99e65f3f14a889b11cd6761
Mauro-Roncata <mauroroncata@gmail.com>
2025-09-22 21:47:34 -0300

    perf: consultas.py

--- Aula7/consultas.py
+++ Aula7/consultas.py
@@ -1,12 +1,12 @@
 from conexao import usar_conexao
 
 def listar_bancos(connection):
-    print("BD existentes no servidor: ")
-    show_db_query = "SHOW DATABASES"
             #Cria o objeto cursor a partir da conexão
     with connection.cursor() as cursor:
                 #Executa a query
-                cursor.execute(show_db_query)
+                cursor.execute("SHOW DATABASES")
                 #Percorre o resultado da query
                 for db in cursor:
                     print(db[0])
+
+usar_conexao(listar_bancos)
