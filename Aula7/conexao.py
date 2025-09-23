6e10198c704647e2e1fee8355d5475f88e95a3b7
jawc-05 <joaoalfredowc@gmail.com>
2025-09-22 21:34:17 -0300

    feat: adding show databases query to python script

--- Aula7/conexao.py
+++ Aula7/conexao.py
@@ -9,5 +9,15 @@ try:
     ) as connection:
         print("Conexão estabelecida!")
 
+        print("BD existentes no servidor: ")
+        show_db_query = "SHOW DATABASES"
+        #Cria o objeto cursor a partir da conexão
+        with connection.cursor() as cursor:
+            #Executa a query
+            cursor.execute(show_db_query)
+            #Percorre o resultado da query
+            for db in cursor:
+                print(db[0])
+
 except:
     print("Ocorreu um erro ao tentar se conectar ao banco de dados.")
