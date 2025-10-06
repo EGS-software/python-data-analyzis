a7dda19241656ce6ecfff296e25d4fe607672c72
Mauro-Roncata <mauroroncata@gmail.com>
2025-10-06 20:34:09 -0300

    feat: adding def user_conexao_mongo

---
+++ Aula8/main.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+def user_conexao_mongo(acao, uri="mongodb://localhost:27017", dbname="dbven"):
+    try:
+        with MongoClient(uri) as client:
+            db = client [dbname]
+            print (f"Conectado a {uri} / db={dbname}") 
+            return acao(db)
+
+    except Exception as e:
+
