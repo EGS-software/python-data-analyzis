fa3071a0cf3e6114e68301eb29239c784bca178b
jawc-05 <joaoalfredowc@gmail.com>
2025-10-06 21:44:21 -0300

    refac: transforming selectAll def to a file .py

--- Aula8/select_mongo.py
+++ Aula8/select_mongo.py
@@ -8,22 +8,5 @@ def selectInput(db):
     for doc in colecao.find().limit(5):
         print(doc)
 
-def selectAll(db):
-    colecctions = ["vendedores", "produtos", "clientes", "vendas", "itensVenda"]
-    for col in colecctions:
-        print(f"\n--- {col} (5 documentos)---")
-        # find({}) => retorna todos os docs
-        #limit(5) => restring 5 docs
-        cursor=db[col].find({}).limit(5)
-
-        docs = list(cursor)
-
-        #Se a coleçaão estiver vazia, informa
-        if not docs:
-            print("Sem documentos")
-        else: 
-            for d in docs:
-                print(d)
 
 user_conexao_mongo(selectInput)
-user_conexao_mongo(selectAll)
