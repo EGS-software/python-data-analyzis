3c4c399f1ec03f69cb0c7719f3ba8164bcaae453
jawc-05 <joaoalfredowc@gmail.com>
2025-10-06 21:40:14 -0300

    refac: changing name from consultas to consults

--- Aula8/select_mongo.py
+++ Aula8/select_mongo.py
@@ -1,7 +1,7 @@
 # Objetivo: SELECT * FROM <tabela> LIMIT 5
 
 from connection_mongo import user_conexao_mongo
-from consultas import listar
+from Aula8.consults import listar
 
 def selecionar(db):
     colecao = db[input("Coleção: ")]
