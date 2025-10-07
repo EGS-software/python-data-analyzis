760c62a28e3adcaa7ca84bde206a37b6178ac327
jawc-05 <joaoalfredowc@gmail.com>
2025-10-06 21:44:59 -0300

    perf: removing useless imports

--- Aula8/select_mongo.py
+++ Aula8/select_mongo.py
@@ -1,7 +1,6 @@
 # Objetivo: SELECT * FROM <tabela> LIMIT 5
 
 from connection_mongo import user_conexao_mongo
-from Aula8.consults import listar
 
 def selectInput(db):
     colecao = db[input("Coleção: ")]
