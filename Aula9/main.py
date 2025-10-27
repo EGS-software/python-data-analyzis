69cca62b3a9bf6f4c917e24d0ef9abbc0d84dadd
Mauro-Roncata <mauroroncata@gmail.com>
2025-10-27 19:39:46 -0300

    feat: adding array primeira_semana

--- Aula9/main.py
+++ Aula9/main.py
@@ -4,3 +4,10 @@ import numpy as np
 cidades = np.array["Augusto Pestana", "Santa Rosa", "Ijuí"]
 
 dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+
+# Matriz 3x7 com temperaturas fictícias
+primeira_semana = np.array([
+    [20,21,23,26,28,27,25],
+    [16,18,21,25,27,26,22],
+    [17,19,22,24,26,25,21]
+])
