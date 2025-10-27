0edad9c32fe78217f1f212a87c83c73fa95f67c3
jawc-05 <joaoalfredowc@gmail.com>
2025-10-27 19:34:28 -0300

    feat: adding array

--- Aula9/main.py
+++ Aula9/main.py
@@ -1,1 +1,6 @@
 import numpy as np
+
+# Vetor de nomes das cidades
+cidades = np.array["Augusto Pestana", "Santa Rosa", "Ijuí"]
+
+dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
