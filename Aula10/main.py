d7643cb5dcccb2e0334f4885cbc83b41ab2e4583
jawc-05 <joaoalfredowc@gmail.com>
2025-12-01 19:51:28 -0300

    style: adding consult dolar

--- Aula10/main.py
+++ Aula10/main.py
@@ -4,3 +4,17 @@ import requests
 response = requests.get("https://viacep.com.br/ws/98700000/json/")
 print(response.status_code)
 print(response.json())
+
+
+#func to consult dollar price
+def consultar_dolar():
+    url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    response = requests.get(url)
+    dados = response.json()
+    # A AwesomeAPI retorna um dicionário com a chave "USDBRL"
+    cotacao = dados.get( "USDBRL")
+    print( f"Conversão : {cotacao.get( 'name', '')}")
+    print( f"Compra : {cotacao.get( 'bid', '')}")
+    print( f"Venda : {cotacao.get( 'ask', '')}")
+    print( f"Atualizado em: {cotacao.get( 'create_date' , '')}")
+consultar_dolar()
