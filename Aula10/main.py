f6f79653eab4b890d06f32c90a92d570a7869c42
jawc-05 <joaoalfredowc@gmail.com>
2025-12-01 19:43:44 -0300

    style: adding comments

--- Aula10/main.py
+++ Aula10/main.py
@@ -1,4 +1,6 @@
 import requests
+
+# using get to consume the api from viacep.com.br#
 response = requests.get("https://viacep.com.br/ws/98700000/json/")
 print(response.status_code)
 print(response.json())
